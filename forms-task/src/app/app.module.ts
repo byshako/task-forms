bc211ca461405935622424c41de1f68b4c912550
byshako <byshako@gmail.com>
2017-11-07 12:17:33 +0400

    Summary
    Comments Components

--- forms-task/src/app/app.module.ts
+++ forms-task/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommentsListComponent } from './comments/comments-list/comments-list.component';
+import { CommentComponent } from './comments/comment/comment.component';
+import { CommentsFormComponent } from './comments/comments-form/comments-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentsListComponent,
+    CommentComponent,
+    CommentsFormComponent
   ],
   imports: [
     BrowserModule
